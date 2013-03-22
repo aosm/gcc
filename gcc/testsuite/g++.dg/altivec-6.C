1db5329838b4732449d58c4e1232bc28025c38b4
Rasmus Sten <rasmus@dll.nu>
2013-03-22 11:28:33 +0200

    version 4061

--- gcc/testsuite/g++.dg/altivec-6.C
+++ gcc/testsuite/g++.dg/altivec-6.C
@@ -1,22 +1,14 @@
-/* APPLE LOCAL file AltiVec */
-/* { dg-do run { target powerpc-apple-darwin* } } */
-/* { dg-options "-faltivec" } */
+/* { dg-do run { target powerpc*-*-* } } */
+/* { dg-options "-maltivec -pedantic" } */
 
-/* Test for correct handling of AltiVec initializers (which
-   look like comma expressions).  */
+int main()
+    {
+    typedef        unsigned char  UC;
+    typedef vector unsigned char  VUC;
 
-extern void abort(void);
-#define CHECK_IF(expr) if(!(expr)) abort()
-
-typedef vector signed int intvec;
-struct vfoo { int x; vector signed int v; int y; };
-
-struct vfoo vx_g = { (short)(10, 15, 20), (intvec)(11, 12, (9, 13), 14), (int)(15, 16) };
-
-int main(void) {
-  signed int *vec = reinterpret_cast<signed int *>(&vx_g.v);
-  CHECK_IF(vx_g.x == 20 && vx_g.y == 16);
-  CHECK_IF(vec[0] == 11 && vec[1] == 12 && vec[2] == 13 && vec[3] == 14);
-  return 0;
-}
-
+    const UC   kBar  = 7;
+    VUC        vBar1 = (VUC)(kBar);
+    VUC        vBar2 =      {kBar};
+    VUC        vBar3 = (VUC){kBar};
+    return 0;
+    }
